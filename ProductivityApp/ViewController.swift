e33fc8cbff19fb80eec8db2559cc797923f34d79
zhanserikAmangeldi <amangeldi.janserik2017@gmail.com>
2025-05-04 03:22:08 +0500

    Init

---
+++ ProductivityApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProductivityApp
+//
+//  Created by Zhanserik Amangeldi on 04.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
