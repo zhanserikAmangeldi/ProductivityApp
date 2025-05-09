3d50ee296215104f8ad556411325bd548ebe1d46
zhanserikAmangeldi <amangeldi.janserik2017@gmail.com>
2025-05-09 19:46:06 +0500

    add TodoTask Model

---
+++ ProductivityApp/TodoList/TodoTask+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  TodoTask+CoreDataClass.swift
+//  ProductivityApp
+//
+//  Created by Zhanserik Amangeldi on 09.05.2025.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TodoTask)
+public class TodoTask: NSManagedObject {
+
+}
